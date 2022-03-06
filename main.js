a44455be756d2b133f7f4862c66adeb6936f600b
SarahKamunya <sarahkamunya254@gmail.com>
2022-03-06 16:53:37 +0300

    Add main.js file

---
+++ main.js
@@ -0,0 +1,14 @@
+/*const btn = document.getElementById("submitButton")
+btn.addEventListener("click" , (e) {
+    e.preventDefault()
+
+    let myDOB = document.getElementById("date").value;
+    let  myGender = document.getElementById("gender-id"). value;
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let maleName =["Kwasi","Kwadwo","Kwaku","Yaw","Kofi","Kwame"];
+    let femaleName =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+
+}) */
+
+
