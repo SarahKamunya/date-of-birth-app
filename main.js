b055a653cb9dc76b5a33b69bef1633afaf2a51bd
SarahKamunya <sarahkamunya254@gmail.com>
2022-03-06 22:34:07 +0300

    Add myGender if else statement pass weekDays

--- main.js
+++ main.js
@@ -3,14 +3,27 @@ btn.addEventListener("click" , (e) =>{
     e.preventDefault()
 
     let myDOB = document.getElementById("date").value;
-    let  myGender = document.getElementById("gender-id"). value;
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let  myGender = document.getElementById("gender-id").value;
+    //let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let maleName =["Kwasi","Kwadwo","Kwaku","Yaw","Kofi","Kwame"];
     let femaleName =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     let myDateBirth = new Date(myDOB);
     let weekDays =myDateBirth.getDay();
 
 
-})
+    //let date = myDateBirth.split('/');
 
+    //myDateBirth.toLocaleDateString('en-US', {weekDays
+    //:'long',}),
+
+
+    
+if (myGender ==1) {
+
+  console.log( maleName[weekDays]);
+
+
+}
+
+})
 
