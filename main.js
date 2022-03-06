7915b9215089f0847bc29bbb43da94ea48b79239
SarahKamunya <sarahkamunya254@gmail.com>
2022-03-06 23:35:05 +0300

    Add background image and style it to fit page, repositioned form

--- main.js
+++ main.js
@@ -21,14 +21,14 @@ btn.addEventListener("click" , (e) =>{
     
 if (myGender ==1) {
 
-  message.innerHTML = `Your Akan Name is ${maleName[weekDays]}`;
+  message.innerHTML = `Your Akan Name is ${maleName[weekDays]}!`;
 
 
 } 
 
 else if (myGender ==2) {
 
-    message.innerHTML = `Your Akan Name is ${femaleName[weekDays]}`;
+    message.innerHTML = `Your Akan Name is ${femaleName[weekDays]}!`;
   
 
 }
